52c20de719c7ef5ccdaba70773eda1491dd9c9d7
Matteo Bruni <ar3s@icloud.com>
2020-07-29 10:55:27 +0200

    template prepared

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
+u
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
+import VueRouter from "vue-router";
 import App from './App.vue'
+import Home from "@/components/Home";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import VueParticles from "particles.vue";
+
+Vue.use(VueRouter);
+Vue.use(VueParticles);
 
 Vue.config.productionTip = false
 
+const routes = [
+    { path: '/', component: Home },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register }
+]
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router
 }).$mount('#app')
